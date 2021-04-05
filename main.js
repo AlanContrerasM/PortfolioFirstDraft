912cd943d34bf70c868e86efd9f1c32d5260c77e
Alan Contreras <alanrcontrerasm@gmail.com>
2021-04-05 12:35:05 -0700

    Create some animations

--- main.js
+++ main.js
@@ -7,20 +7,47 @@ let i = 0;
 
 let typing = setInterval(function () {
   let displayText = greetingArr.slice(0, i + 1).join("");
-  //ternary operator, let's try to make the leading cursor flash.
+  //ternary operator, let's try to make the leading cursor flash. Meh, didn't look good
   //   let leading = i % 2 == 0 ? "_" : " ";
   helloGreeting.innerHTML = displayText;
   i++;
   if (i >= greetingArr.length) {
     createFloaterDiv();
+    floaterClick();
     clearInterval(typing);
   }
 }, 350);
 
 //making the floater appear
-
 function createFloaterDiv(){
     const floaterDiv = document.getElementById("floaterDiv");
     floaterDiv.classList.toggle("invisible");
+}
+
+//easter egg on floaterDiv, need to press. Maybe you'll find it, maybe not!
+
+function floaterClick(){
+
+    //dissappearing the big floaterDiv
+    const floaterDiv = document.getElementById("floaterDiv");
+    floaterDiv.addEventListener("click", (e)=>{
+        e.target.classList.toggle("invisible");
+        createFloaters();
+    })
+
+}
+
+function createFloaters(){
+    //making the floaters appear with a delay for each one
+
+    const floaters = document.querySelectorAll(".floaters");
+
+    floaters.forEach((floaty, index)=>{
+        console.log(index);
+        setTimeout(function(){
+            console.log("creating", floaty)
+    floaty.classList.toggle("invisible");
+        }, 1500 * index);
+    })
 }
 
