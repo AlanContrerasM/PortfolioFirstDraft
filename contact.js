ec143cd4195fcf6f849c281ca63ae8f5ea3e74dc
Alan Contreras <alanrcontrerasm@gmail.com>
2021-04-10 14:29:20 -0700

    Add mobile version and mobile nav functionality

--- contact.js
+++ contact.js
@@ -11,6 +11,56 @@ DropDMenus.forEach((menu) => {
   });
 });
 
+
+//mobile menu functionality
+const nav = document.getElementsByTagName("nav")[0];
+const navIcon = document.getElementById("navIcon");
+const closeNav = document.getElementById("closeNav");
+
+navIcon.addEventListener("click", (e)=>{
+  nav.style.height = "100%";
+  navIcon.classList.add("invisible");
+  closeNav.classList.remove("invisible");
+})
+
+closeNav.addEventListener("click", (e)=>{
+  nav.style.height = "0%";
+  navIcon.classList.remove("invisible");
+  closeNav.classList.add("invisible");
+
+})
+
+//there is a problem with the navIcon, since it's set to invisible class at first
+//we need a media query that works with javascript.
+//reference=https://css-tricks.com/working-with-javascript-media-queries/
+
+//check for mobile size
+const mediaQueryMobile = window.matchMedia('(max-width: 768px)');
+const mediaQueryNormal = window.matchMedia('(min-width: 768px)');
+
+//handle start of page, if we should show icon
+if(mediaQueryMobile.matches){
+  navIcon.classList.remove("invisible");
+}
+
+//handle changes when user changes screen.
+function handleMobileScreen(e){
+  if(e.matches){
+    navIcon.classList.remove("invisible");
+  }
+}
+
+function handleNormalScreen(e){
+  if(e.matches){
+    navIcon.classList.add("invisible");
+  }
+}
+
+//setting event listeners.
+mediaQueryMobile.addEventListener("change", handleMobileScreen);
+mediaQueryNormal.addEventListener("change", handleNormalScreen);
+
+
 //form stuff
 let form = document.getElementsByTagName("form")[0];
 let inputs = document.querySelectorAll(".contactFormEl");
